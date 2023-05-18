8c32f84c1fc85621399c2cb212d06618bf21a940
qgsoriano1 <112040935+qgsoriano1@users.noreply.github.com>
2023-05-18 20:06:10 +0800

    Add files via upload

--- streamlit_soriano.py
+++ streamlit_soriano.py
@@ -16,7 +16,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-    model=tf.keras.models.load_model('bestbest_model.h5')
+  model=tf.keras.models.load_model('bestbest_model.h5')
   return model
 model=load_model()
 st.write("""
